3f19ccb75268202c331375715a6d5f529c3baedf
lishihui <451959050@qq.com>
2017-06-12 14:39:16 +0800

    first commit

---
+++ Simple/rsvlib/src/main/java/com/ebanswers/lsh/RingStatisticsView.java
@@ -0,0 +1,211 @@
+package com.ebanswers.lsh;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
+/**
+ * @author
+ * Created by lishihui on 2016/12/26.
+ */
+
+public class RingStatisticsView extends View {
+    private float[] mPercent = new float[]{0.1f, 0.2f, 0.3f, 0.4f};
+    private int[] mColors = new int[]{Color.parseColor("#F9AA28"), Color.parseColor("#009752"), Color.parseColor("#2EC1FB"), Color.parseColor("#FA6723")};
+    private Paint mPaint, mLinePaint, mPathPaint;
+    private float mRingWidth = 30;
+    private int textSize1 = 15, textSize2 = 18, textSize3 = 15;
+    private TextPaint mTextPaint;
+    private int mTextColor1 = Color.parseColor("#BBB9B8"), mTextColor2 = Color.parseColor("#FC824B");
+    private RectF mRectF;
+    private String mstr_total_text = "总资产";
+    private String mstr_total_number = "10000.00";
+    private int width, height;
+    private int radius;
+
+    public RingStatisticsView(Context context) {
+        super(context);
+        init();
+    }
+
+    public RingStatisticsView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public RingStatisticsView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(mRingWidth);
+
+        mLinePaint = new Paint(mPaint);
+
+        mPathPaint = new Paint(mPaint);
+        mPathPaint.setStrokeWidth(1);
+
+        mTextPaint = new TextPaint();
+        mTextPaint.setAntiAlias(true);
+
+        mRectF = new RectF();
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int size = Math.max(MeasureSpec.getSize(widthMeasureSpec), MeasureSpec.getSize(widthMeasureSpec));
+        setMeasuredDimension(size, size);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        width = getWidth();
+        height = getHeight();
+        radius = Math.max(width, height) / 4;
+        mRectF.set(getPaddingLeft() + mRingWidth / 2 + radius, getPaddingTop() + mRingWidth / 2 + radius, width - getPaddingRight() - mRingWidth / 2 - radius, getHeight() - getPaddingBottom() - mRingWidth / 2 - radius);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        float sweepAngle = 0f;
+        float startAngle = 180f;
+        float angle = 0f;
+        float startX, startY, centerX, centerY, endX, endY, textX;
+        int mCount = mPercent.length;
+		mPaint.setStrokeWidth(mRingWidth);
+		mLinePaint.setStrokeWidth(mRingWidth);
+        for (int i = 0; i < mCount; i++) {
+            mPaint.setColor(mColors[i]);
+            startAngle += sweepAngle;
+            sweepAngle = 360 * mPercent[i];
+            canvas.drawArc(mRectF, startAngle, sweepAngle, false, mPaint);
+            angle = (startAngle - 180) + sweepAngle / 2;
+            startX = (float) (width / 2 - radius * Math.cos(angle * Math.PI / 180));
+            startY = (float) (height / 2 - radius * Math.sin(angle * Math.PI / 180));
+            if (angle <= 90) {
+                centerX = startX - width / 8;
+                centerY = startY - height / 8;
+                endX = centerX - width / 8;
+                endY = centerY;
+                textX = centerX - width / 16;
+            } else if (angle <= 180) {
+                centerX = startX + width / 8;
+                centerY = startY - height / 8;
+                endX = centerX + width / 8;
+                endY = centerY;
+                textX = centerX + width / 16;
+            } else if (angle <= 270) {
+                centerX = startX + width / 8;
+                centerY = startY + height / 8;
+                endX = centerX + width / 8;
+                endY = centerY;
+                textX = centerX + width / 16;
+            } else {
+                centerX = startX - width / 8;
+                centerY = startY + height / 8;
+                endX = centerX - width / 8;
+                endY = centerY;
+                textX = centerX - width / 16;
+            }
+            Path path = new Path();
+            path.moveTo(startX, startY);
+            path.lineTo(centerX, centerY);
+            path.lineTo(endX, endY);
+            mPathPaint.setColor(mColors[i]);
+            canvas.drawPath(path, mPathPaint);
+            DecimalFormat df = new DecimalFormat("######0.0");
+            BigDecimal b = new BigDecimal(mPercent[i] * 100);
+            double f1 = b.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+            String percent = df.format(f1)+ "%";
+            float text_width = mTextPaint.measureText(percent);
+            mTextPaint.setColor(mColors[i]);
+            mTextPaint.setTextSize(textSize3);
+            canvas.drawText(percent, textX - text_width / 2, endY - (mTextPaint.getFontMetrics().descent - mTextPaint.getFontMetrics().ascent) / 2, mTextPaint);
+        }
+        mLinePaint.setColor(Color.WHITE);
+        for (int i = 0; i < mCount; i++) {
+            if (i == 0) {
+                angle = 179;
+            } else {
+                angle += 360 * mPercent[i - 1];
+            }
+            canvas.drawArc(mRectF, angle, 2, false, mLinePaint);
+        }
+
+        mTextPaint.setTextSize(textSize1);
+        float textwidth1 = mTextPaint.measureText(mstr_total_text);
+        mTextPaint.setColor(mTextColor1);
+        canvas.drawText(mstr_total_text, width / 2 - textwidth1 / 2, (height / 2 - (mTextPaint.getFontMetrics().descent - mTextPaint.getFontMetrics().ascent) / 2), mTextPaint);
+        mTextPaint.setTextSize(textSize2);
+        float textwidth2 = mTextPaint.measureText(mstr_total_number);
+        mTextPaint.setColor(mTextColor2);
+        canvas.drawText(mstr_total_number, width / 2 - textwidth2 / 2, (height / 2 + (mTextPaint.getFontMetrics().descent - mTextPaint.getFontMetrics().ascent) / 2) + 2, mTextPaint);
+        canvas.translate(getWidth() / 2, getHeight() / 2);
+        canvas.save();
+    }
+
+    public void setPercentAndColors(float[] percent, int[] colors) {
+        if (percent != null && colors != null) {
+            if (percent.length == colors.length) {
+                mPercent = percent;
+                mColors = colors;
+            } else {
+                throw new IllegalArgumentException("length of percent must equals length of colors");
+            }
+        }
+    }
+
+    public void setRingWidth(float ringWindth) {
+        mRingWidth = ringWindth;
+    }
+
+    public void setCenterText(String text) {
+        mstr_total_text = text;
+    }
+
+    public void setCenterNumber(String number) {
+        mstr_total_number = number;
+    }
+
+    public void setCenterTextColor(int color) {
+        mTextColor1 = color;
+    }
+
+    public void setCenterNumberColor(int color) {
+        mTextColor2 = color;
+    }
+
+    public void setCenterTextSize(int size) {
+        textSize1 = size;
+    }
+
+    public void setCenterNumberSize(int size) {
+        textSize2 = size;
+    }
+
+    public void setPercentTextSize(int size) {
+        textSize3 = size;
+    }
+
+    public void refresh() {
+        postInvalidate();
+    }
+}
